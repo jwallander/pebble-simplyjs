99fd7563849a9b514b178fae3a03ebc7e1967cdb
jwallander <jwallander@gmail.com>
2014-05-30 13:16:05 +0200

    Update pebblesimplyjw.js

--- pebblesimplyjw.js
+++ pebblesimplyjw.js
@@ -1,19 +1,15 @@
 console.log('Simply.js demo!');
 
 simply.on('singleClick', function(e) {
-  console.log(util2.format('single clicked $button!', e));
-  simply.subtitle('Pressed ' + e.button + '!');
-});
-
-simply.on('longClick', function(e) {
-  console.log(util2.format('long clicked $button!', e));
-  simply.vibe();
-  simply.scrollable(e.button !== 'select');
-});
-
-simply.on('accelTap', function(e) {
-  console.log(util2.format('tapped accel axis $axis $direction!', e));
-  simply.subtitle('Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+  if (e.button === 'down') {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      var coords = pos.coords;
+      var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+      'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+      ajax({ url: weatherUrl, type: 'json' }, function(data) {
+        simply.text({ title: data.name, subtitle: data.main.temp });
+    });
+  }
 });
 
 simply.setText({
