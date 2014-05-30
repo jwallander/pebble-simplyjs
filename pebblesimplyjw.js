20afafbd4d31690441488b01f1a5d774fbb01e74
jwallander <jwallander@gmail.com>
2014-05-30 13:21:49 +0200

    Update pebblesimplyjw.js

--- pebblesimplyjw.js
+++ pebblesimplyjw.js
@@ -7,7 +7,7 @@ simply.on('singleClick', function(e) {
       var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
       'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
       ajax({ url: weatherUrl, type: 'json' }, function(data) {
-        simply.text({ title: data.name, subtitle: data.main.temp });
+        simply.text({ title: data.name, subtitle: data.main.temp }, true);
       });
     });
   }
